ebf52640e41c5b6d0cc7f5a4b12d2d9371de63cc
David <albertdavidlopezpinzon@gmail.com>
2025-10-03 23:44:41 -0500

    Implemetnacion de Transaccion

--- src/models/Transaccion.java
+++ src/models/Transaccion.java
@@ -1,5 +1,25 @@
 package models;
+import java.time.LocalDateTime;
 
 public class Transaccion {
-   
+    private String idTransaccion;
+    private String tipo; //deposito o retiro
+    private double monto;
+    private LocalDateTime fecha;
+
+
+    public Transaccion(String idTransaccion, String tipo, double monto, LocalDateTime fecha) {
+        this.idTransaccion = idTransaccion;
+        this.tipo=tipo;
+        this.monto=monto;
+        this.fecha=fecha;
+
+    }
+
+    //tostring
+    @Override
+    public String toString(){
+        return "Transaccion{" + "idTransaccion='" + idTransaccion + '\'' + ", tipo='" + tipo + '\'' + ", monto=" + monto + ", fecha=" + fecha + '}';
+    }
+
 }

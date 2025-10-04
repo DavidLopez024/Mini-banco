9208352a4556849c71ae330ce0fc738a8fb22554
David <albertdavidlopezpinzon@gmail.com>
2025-10-03 23:00:50 -0500

    Implementacion solo usuario

--- src/models/Transaccion.java
+++ src/models/Transaccion.java
@@ -1,19 +1,5 @@
 package models;
 
 public class Transaccion {
-    private String tipo;
-    private double monto;
-
-    public Transaccion(String tipo, double monto) {
-        this.tipo = tipo;
-        this.monto = monto;
-    }
-
-    public String getTipo() { return tipo; }
-    public double getMonto() { return monto; }
-
-    @Override
-    public String toString() {
-        return "Transaccion{" + "tipo='" + tipo + '\'' + ", monto=" + monto + '}';
-    }
+   
 }
